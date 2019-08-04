aacfec725e96705f0ce2fc63c11a3b3db6e2b3a5
Patrick A <akolo@hotmail.com>
2019-08-04 11:00:55 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,6 +8,12 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
     end
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
   
 sql = <<-SQL
